256f068bac072bbe99a9fe93290953ef0585540a
emmajblee <153022546+emmajblee@users.noreply.github.com>
2024-05-12 17:03:52 -0400

    individual toggle fix

--- js/index.js
+++ js/index.js
@@ -3,17 +3,20 @@ console.log ("your index.js file is loaded correctly");
 $(document) .ready(function(){
     $(".Card1").click(function(){
         $(".CardPanel1").slideToggle("slow");
+        $(".Card1 img").toggleClass("toggleArrow")
     })
 })
 
 $(document) .ready(function(){
     $(".Card2").click(function(){
         $(".CardPanel2").slideToggle("slow");
+        $(".Card2 img").toggleClass("toggleArrow")
     })
 })
 
 $(document) .ready(function(){
     $(".Card3").click(function(){
         $(".CardPanel3").slideToggle("slow");
+        $(".Card3 img").toggleClass("toggleArrow")
     })
 })
