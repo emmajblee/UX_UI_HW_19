8d538ddb08ae0c8578b6087ec7903f8715233f87
emmajblee <153022546+emmajblee@users.noreply.github.com>
2024-05-12 16:42:12 -0400

    toggle fix

--- js/index.js
+++ js/index.js
@@ -2,18 +2,18 @@ console.log ("your index.js file is loaded correctly");
 
 $(document) .ready(function(){
     $(".Card1").click(function(){
-        $(".CardPanel1").slideDown("slow");
+        $(".CardPanel1").slideToggle("slow");
     })
 })
 
 $(document) .ready(function(){
     $(".Card2").click(function(){
-        $(".CardPanel2").slideDown("slow");
+        $(".CardPanel2").slideToggle("slow");
     })
 })
 
 $(document) .ready(function(){
     $(".Card3").click(function(){
-        $(".CardPanel3").slideDown("slow");
+        $(".CardPanel3").slideToggle("slow");
     })
 })
